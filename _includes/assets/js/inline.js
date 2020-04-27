e9111df41b143636dbaebea634832575e69049f4
David Rhoden <david@davidrhoden.com>
2020-04-27 13:28:06 -0500

    FAQ styling and JS

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,17 +1,28 @@
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
-      });
-    }
-  });
-}
+// if (window.netlifyIdentity) {
+//   window.netlifyIdentity.on("init", user => {
+//     if (!user) {
+//       window.netlifyIdentity.on("login", () => {
+//         document.location.href = "/admin/";
+//       });
+//     }
+//   });
+// }
 
 $(document).ready(function(){
   var $hamburger = $(".hamburger");
   $hamburger.on("click", function(e) {
     $hamburger.toggleClass("is-active");
     $("nav").toggleClass("is-active");
+  });
+});
+
+$(document).ready(function(){
+  var allPanels = $('.answer').hide();
+    
+  $('.accordion > .question').click(function() {
+    allPanels.slideUp();
+    $(this).toggleClass("is-active");
+    $(this).next().toggleClass("is-active");
+    return false;
   });
 });
